328b1d80dd991efa71357b98a40433dd5d929bf8
Aaron VonderHaar <av@noredink.com>
2018-08-30 17:07:06 -0700

    Upgrade to Elm 0.19

--- examples/Examples.elm
+++ examples/Examples.elm
@@ -1,14 +1,15 @@
-module Main exposing (..)
+module Main exposing (main)
 
-import Transducer exposing (..)
-import Transducer as T exposing ((>>>))
-import Transducer.Debug exposing (..)
-import Html exposing (Html)
-import Set
 import Array
-import Mouse
-import String
+import Browser
+import Browser.Events
+import Html exposing (Html)
+import Json.Decode
 import Result
+import Set
+import String
+import Transducer as T exposing (..)
+import Transducer.Debug exposing (..)
 
 
 double : Transducer a a r ()
@@ -49,29 +50,28 @@ double_ =
 
 combined =
     filter (\x -> x >= 3)
-        >>> map toString
-        >>> double
-        >>> take 3
-        >>> generate "999"
-        >>> generate "777"
-        >>> partition 3
+        |> composeWith (map Debug.toString)
+        |> composeWith double
+        |> composeWith (take 3)
+        |> composeWith (generate "999")
+        |> composeWith (generate "777")
+        |> composeWith (partition 3)
 
 
 combined_ =
     debug "combined" combined
 
 
-parseValidInts : Transducer String Int r ( ( ( (), () ), () ), () )
+parseValidInts : Transducer String Int r ( ( (), () ), () )
 parseValidInts =
     T.map String.toInt
-        >>> T.map Result.toMaybe
-        >>> T.filter ((/=) Nothing)
-        >>> T.map (Maybe.withDefault 0)
+        |> composeWith (T.filter ((/=) Nothing))
+        |> composeWith (T.map (Maybe.withDefault 0))
 
 
 show : a -> Html msg
 show a =
-    Html.text <| toString a
+    Html.text <| Debug.toString a
 
 
 flowDown : List (Html msg) -> Html msg
@@ -85,30 +85,37 @@ render e =
     flowDown
         [ e
         , show <| transduceList (take 2) [ "A", "B", "C", "D" ]
-        , show <| transduceList (map toString) [ 1, 2, 3, 4 ]
+        , show <| transduceList (map String.fromInt) [ 1, 2, 3, 4 ]
         , show <| transduceList (filter (\x -> x >= 3)) [ 1, 5, 2, 3, 4 ]
-        , show <| transduceList (double) [ "A", "X", "B" ]
+        , show <| transduceList double [ "A", "X", "B" ]
         , show <| transduceList combined [ 1, 2, 3, 4 ]
         , show <| transduceArray (take 2) (Array.initialize 5 identity)
-        , show <| transduceArray (take 2 >>> map toString) (Array.initialize 5 identity)
-          --, show <| transduceArray combined (Array.initialize 5 identity)
-          --, show <| transduce List.foldr Set.insert (Set.singleton "9") combined [1, 2, 3, 4]
-          --, show <| transduce Set.foldr Set.insert Set.empty combined (Set.fromList [8])
-        , show <| transduce List.foldr (+) 0 (comp double (generate 100)) [ 1, 2, 3 ]
-        , show <| transduce Set.foldr (+) 0 (comp double (generate 100)) (Set.fromList [ 1, 2, 3 ])
+        , show <| transduceArray (take 2 |> composeWith (map String.fromInt)) (Array.initialize 5 identity)
+
+        --, show <| transduceArray combined (Array.initialize 5 identity)
+        --, show <| transduce List.foldr Set.insert (Set.singleton "9") combined [1, 2, 3, 4]
+        --, show <| transduce Set.foldr Set.insert Set.empty combined (Set.fromList [8])
+        , show <| transduce List.foldr (+) 0 (double |> composeWith (generate 100)) [ 1, 2, 3 ]
+        , show <| transduce Set.foldr (+) 0 (double |> composeWith (generate 100)) (Set.fromList [ 1, 2, 3 ])
         , show <| transduceList parseValidInts [ "123", "-34", "35.0", "SDF", "7" ]
         ]
 
 
 mt =
     filter (\{ x, y } -> y <= 100)
-        >>> map show
+        |> composeWith (map show)
+
+
+decodeMouseLocation =
+    Json.Decode.map2 (\x y -> { x = x, y = y })
+        (Json.Decode.at [ "target", "clientX" ] Json.Decode.int)
+        (Json.Decode.at [ "target", "clientY" ] Json.Decode.int)
 
 
 main =
-    Html.program
-        { init = ( Html.text "", Cmd.none )
-        , subscriptions = \_ -> Mouse.moves identity
+    Browser.element
+        { init = \() -> ( Html.text "", Cmd.none )
+        , subscriptions = \_ -> Browser.Events.onMouseMove decodeMouseLocation
         , update =
             \a _ ->
                 ( transduceList mt [ a ]
--- src/Transducer/Debug.elm
+++ src/Transducer/Debug.elm
@@ -1,8 +1,10 @@
-module Transducer.Debug exposing (..)
+module Transducer.Debug exposing (debug)
 
 {-| This module provides support for debugging transducers.
 
+
 # Logging
+
 @docs debug
 
 -}
@@ -14,8 +16,14 @@ import Transducer exposing (..)
 {-| Wrap an existing transducer such that input and output of the transducer
 will be logged with `Debug.log`.
 
-    filter' pred = debug "filter" (filter pred)
-    main = show (transduceList (filter' ((/=) 2)) [1, 2, 3])
+
+    filter' pred =
+        debug "filter" (filter pred)
+
+    main =
+        show (transduceList (filter' ((/=) 2)) [ 1, 2, 3 ])
+
+
     -- Console output:
     --     filter: input: 3
     --     filter: state: ()
@@ -26,6 +34,7 @@ will be logged with `Debug.log`.
     --     filter: state: ()
     --     filter -> : 1
     --     filter: complete: ()
+
 -}
 debug : String -> Transducer a b r s -> Transducer a b r s
 debug name t =
@@ -42,11 +51,11 @@ debug name t =
         logProxy reduce input =
             reduce (Debug.log (name ++ " -> ") input)
     in
-        { init = t.init
-        , step =
-            \reduce input ( s, r ) ->
-                (logProxy reduce |> t.step) (logInput input) ( logState s, r )
-        , complete =
-            \reduce ( s, r ) ->
-                t.complete (logProxy reduce) ( logComplete s, r )
-        }
+    { init = t.init
+    , step =
+        \reduce input ( s, r ) ->
+            (logProxy reduce |> t.step) (logInput input) ( logState s, r )
+    , complete =
+        \reduce ( s, r ) ->
+            t.complete (logProxy reduce) ( logComplete s, r )
+    }
